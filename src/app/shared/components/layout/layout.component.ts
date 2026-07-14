80f6f2fdca18fc5eed418a809c007fbb3b503b06
mohitvishwakarma011 <mohitvishwakarma681@gmail.com>
2026-07-14 23:49:41 +0530

    Fixed ui issue

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, ViewEncapsulation } from "@angular/core";
 import { AppUtils } from "src/app/helpers/app.utils";
-import { Router } from "@angular/router";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
 import { LoadingBarService } from "@ngx-loading-bar/core";
-import { map, Observable, startWith } from "rxjs";
+import { filter, map, Observable, startWith } from "rxjs";
 import { LoadingObserverService } from "../../services/loading-observer.service";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
     selector: 'ngx-layout',
@@ -14,13 +15,28 @@ import { LoadingObserverService } from "../../services/loading-observer.service"
 export class LayoutComponent {
     protected isAuthenticated: boolean = false;
     private readonly _appUtils: AppUtils = inject(AppUtils);
+    protected isAuthPage = false;
 
     isLoading$: Observable<boolean>;
 
     constructor(private loadingBar: LoadingBarService,
         private loaderObserverService: LoadingObserverService,
-        private readonly _router: Router) {
+        private readonly _router: Router,
+        private readonly _route: ActivatedRoute) {
         this.isAuthenticated = this._appUtils.isUserAuthenticated();
+        _router.events.pipe(
+            filter(event => event instanceof NavigationEnd),
+            takeUntilDestroyed()
+        )
+            .subscribe((event) => {
+                const nav = event as NavigationEnd;
+                if (nav.url.includes("auth")) {
+                    this.isAuthPage = true;
+                } else {
+                    this.isAuthPage = false;
+                }
+                console.log(this.isAuthPage);
+            });
 
         this.isLoading$ = this.loadingBar.value$.pipe(map(value => value > 0 && value < 100), startWith(false));
 
